5b454445aa8c8e738ecbd09ae6317d2f8aa50314
Brayan Rojas <brayan0630@gmail.com>
2016-05-27 14:30:52 -0500

    todas las clases e interfaces creadas2

---
+++ registro.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+      
+        <title>perfil:nombrede usuario</title>
+    </head>
+             
+   
+        
+        <style>
+       div label{
+            width: 25%;
+        }    
+        input{
+            border: solid 4px black;
+        }       
+        
+        </style>    
+     
+            
+    </head>
+    <body>
+        <h1>REGISTRO</h1>
+            <form>
+                <fieldset>
+                     <p>
+                        <input type="submit" value ="proveedor">
+                        
+                        
+                     
+                        <input type="submit" value ="mensajero">
+                        </p>    
+                        
+                    <p>
+                        <label>nombre</label>
+                        <input type="text" name="nombre">
+                        
+                         <label>usuario</label>
+                        <input type="text" name="usuario">
+                    </p>
+                         
+                      
+                        
+                    <p>
+                        <label>confirmar</label>
+                        <input type="text" name="confirmar">
+                    
+                        <label>apellido</label>
+                        <input type="text" name="apellido">
+                        </p>
+                        
+                         <p>
+                        <label>contraseña</label>
+                        <input type="text" name="contraseña">
+                    
+                        <label>numero-cuenta</label>
+                        <input type="text" name="numero-cuenta">
+                        </p>
+                        
+                        
+                        
+                        <p>
+                        <input type="submit" value ="guardar">
+                        </p> 
+                        
+                        
+                            
+  
