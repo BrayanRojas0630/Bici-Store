85d46907d53dce3c9daee354f3062a2c400eec9a
andres56985 <andis2203@hotmail.com>
2016-05-27 13:27:59 -0500

    Add files via upload

---
+++ perfil nombre-usuario.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+      
+        <title>perfil:nombrede usuario</title>
+    </head>
+        
+        <style>
+       div label{
+            width: 25%;
+        }    
+        input{
+            border: solid 4px blue;
+        }       
+        
+        </style>    
+     
+            
+    </head>
+    <body>
+        <h1>PERFIL:NOMBRE DE USUARIO</h1>
+            <form>
+                <fieldset>
+                     <p>
+                        <input type="submit" value ="proveedor">
+                        
+                        
+                     
+                        <input type="submit" value ="mensajero">
+                        </p>    
+                        
+                    <p>
+                        <label>nombre</label>
+                        <input type="text" name="nombre">
+                        
+                         <label>correo</label>
+                        <input type="text" name="correo">
+                    </p>
+                         
+                      
+                        
+                    <p>
+                        <label>confirmar contraseña</label>
+                        <input type="text" name="confirmar contraseña">
+                    
+                        <label>apellido</label>
+                        <input type="text" name="apellido">
+                        </p>
+                        
+                         <p>
+                        <label>contraseña</label>
+                        <input type="text" name="contraseña">
+                    
+                        <label>telefono</label>
+                        <input type="text" name="telefono">
+                        </p>
+                        
+                        
+                        
+                        <p>
+                        <input type="submit" value ="guardar">
+                        </p> 
+                        
+                        
+                            
+        <?php
+       
+        ?>
+    </body>
+</html>
